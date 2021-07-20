0fe4c2b4262bcf5a624476d281c43ecd7774b572
DSMihaylov <h1zzzy66@gmail.com>
2021-07-20 16:51:56 +0300

    добавили экшены под RHEL

--- src/test/java/tests/order/OrderRhelTest.java
+++ src/test/java/tests/order/OrderRhelTest.java
@@ -1,5 +1,6 @@
 package tests.order;
 
+import core.exception.CustomException;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
@@ -20,15 +21,27 @@ public class OrderRhelTest extends Hooks {
     @ParameterizedTest
     @DisplayName("Заказ продуктов с разной комбинацией среды, сегмента, дата-центра и платформы")
     @MethodSource("dataProviderMethod")
-    public void test(String product, String env, String segment, String dataCentre, String platform) throws IOException, ParseException {
+    public void test(String product, String env, String segment, String dataCentre, String platform) throws IOException, ParseException, CustomException {
         OrderServiceSteps orderServiceSteps = new OrderServiceSteps();
         orderServiceSteps.CreateOrderWithOutline(product, env, segment, dataCentre, platform);
+        orderServiceSteps.CheckOrderStatus("success");
+        orderServiceSteps.ExecuteAction("reset_vm");
+        orderServiceSteps.CheckActionStatus("success");
+        orderServiceSteps.ExecuteAction("stop_vm_soft");
+        orderServiceSteps.CheckActionStatus("success");
+        orderServiceSteps.ExecuteAction("start_vm");
+        orderServiceSteps.CheckActionStatus("success");
+        orderServiceSteps.ExecuteAction("stop_vm_hard");
+        orderServiceSteps.CheckActionStatus("success");
+        orderServiceSteps.ExecuteAction("delete_vm");
+        orderServiceSteps.CheckActionStatus("success");
+
     }
 
     static Stream<Arguments> dataProviderMethod() {
-        return Stream.of(Arguments.arguments("Rhel", "DEV", "dev-srv-app", "5", "Nutanix"),
+        return Stream.of(Arguments.arguments("Rhel", "DEV", "dev-srv-app", "5", "vSphere"));/*,
                 Arguments.arguments("Rhel", "DEV", "dev-srv-app", "5", "vSphere"),
                 Arguments.arguments("Apache_Kafka", "DEV", "dev-srv-app", "5", "Nutanix"),
-                Arguments.arguments("Apache_Kafka", "DEV", "dev-srv-app", "5", "vSphere"));
+                Arguments.arguments("Apache_Kafka", "DEV", "dev-srv-app", "5", "vSphere"));*/
     }
 }
