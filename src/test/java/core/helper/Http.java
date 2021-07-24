e4f566ceee8abc37ed299885d756f15d6b4f6e45
DSMihaylov <h1zzzy66@gmail.com>
2021-07-24 20:41:26 +0300

    убрал лог

--- src/test/java/core/helper/Http.java
+++ src/test/java/core/helper/Http.java
@@ -115,7 +115,6 @@ public class Http {
 
     private String getBearerToken() {
         TestVars testVars = LocalThead.getTestVars();
-        System.out.println("token = " + testVars.getVariable("token_type") + " " + testVars.getVariable("token"));
         return testVars.getVariable("token_type") + " " + testVars.getVariable("token");
     }
 
