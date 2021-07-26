131e04a94550e891ad4aee334b0cd381e69a603d
DSMihaylov <h1zzzy66@gmail.com>
2021-07-26 17:40:31 +0300

    Добавил получение proj_env для получения индексов

--- src/test/java/steps/AuthorizerSteps.java
+++ src/test/java/steps/AuthorizerSteps.java
@@ -7,16 +7,13 @@ import io.qameta.allure.Step;
 import io.restassured.path.json.JsonPath;
 import lombok.extern.log4j.Log4j2;
 import org.json.simple.JSONObject;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.List;
-import java.util.Random;
 
 
 @Log4j2
 public class AuthorizerSteps extends Steps {
     private static final String URL = Configurier.getInstance().getAppProp("host_kong");
+    private final PortalBackSteps portalBackSteps = new PortalBackSteps();
 
     @Step("Получение имени организации")
     public void getOrgName(String orgTitle) {
@@ -34,45 +31,6 @@ public class AuthorizerSteps extends Steps {
         System.out.println();
     }
 
-    @Step("Получение информационных систем")
-    public void getInfoSys(String sysName) {
-        JsonPath jsonPath = new Http(URL)
-                .get(String.format("portal/api/v1/organizations/%s/information_systems?page=1&per_page=100&include=total_count", ShareData.getString("orgName")))
-                .assertStatus(200)
-                .jsonPath();
-
-        int countOfIteration = (int)jsonPath.get("meta.total_count")/ 100 + 1;
-
-        for (int i = 1; i <=countOfIteration; i++){
-            JsonPath jsonPathInCycle = new Http(URL)
-                    .get(String.format("portal/api/v1/organizations/%s/information_systems?page=%s&per_page=100&include=total_count", ShareData.getString("orgName"), i))
-                    .assertStatus(200)
-                    .jsonPath();
-
-            if(jsonPathInCycle.get(String.format("list.find{it.code=='%s'}.id", sysName)) != null){
-                ShareData.put("infoSystemId", jsonPathInCycle.get(String.format("list.find{it.code=='%s'}.id", sysName)));
-                break;
-            }
-        }
-    }
-
-    @Step("Создание папки {folder_type} и сохраняем в переменную {param}")
-    public void createBusinessBlock(String folder_type, String param) {
-        log.info("Изменение базового шаблона запроса при создании папки с типом: " + folder_type);
-
-        JsonPath jsonPath = jsonHelper.getJsonTemplate("/structure/create_folder.json")
-                .set("$.folder.kind", folder_type)
-                .set("$.folder.title", folder_type)
-                .send(URL)
-                .post("authorizer/api/v1/organizations/vtb/folders")
-                .assertStatus(201)
-                .jsonPath();
-
-        String title = jsonPath.get("data.title");
-        testVars.setVariables("businessBlockTitle", title);
-        String folder_id = jsonPath.get("data.name");
-        ShareData.put(param, folder_id);
-    }
 
     @Step("Создание папки типа {folder_type} в родительской папке {parentName} с именем {name} и сохраняем в переменную ее ID {param}")
     public void createFolder(String folder_type, String parentName, String name, String param) {
@@ -96,15 +54,13 @@ public class AuthorizerSteps extends Steps {
 
     @Step("Создать проект с именем {project_name} для родительской папки {folder_id}")
     public void createProject(String project_name, String folder_id) {
-        String infoSystems = jsonHelper.getTestDataFieldValue("structure/projectEnvironmentsDEV.json", "DEV", "information_systems");
-        String projectEnvId = jsonHelper.getTestDataFieldValue("structure/projectEnvironmentsDEV.json", "DEV", "project_environment_id");
-        String prefix = getPrefixEnv(folder_id, infoSystems, projectEnvId);
+        String prefix = portalBackSteps.getPrefixEnv(folder_id, ShareData.getString("infoSystemId"), ShareData.getString("ProjEnvId"));
         log.info("Folder ID: " + ShareData.getString(folder_id));
 
         String project_id = jsonHelper.getJsonTemplate("/structure/create_project.json")
                 .set("$.project.title", project_name)
-                .set("$.project.information_system_id", infoSystems)
-                .set("$.project.project_environment_id", projectEnvId)
+                .set("$.project.information_system_id", ShareData.getString("infoSystemId"))
+                .set("$.project.project_environment_id", ShareData.getString("ProjEnvId"))
                 .set("$.project.environment_prefix_id", prefix)
                 .send(URL)
                 .post(String.format("authorizer/api/v1/folders/%s/projects", ShareData.getString(folder_id)))
@@ -117,26 +73,6 @@ public class AuthorizerSteps extends Steps {
         jsonObj.put("id", project_id);
         jsonObj.put("env", "DEV");
         ShareData.putArray("projects", jsonObj);
-    }
-
-    public String getPrefixEnv(String folder_id, String infoSystems, String projectEnvId) {
-        log.info(String.format("Получение префикса для ИС: %s и ProjectEnv: %s для контекста %s", infoSystems, projectEnvId, ShareData.getString(folder_id)));
-        JsonPath jsonPath = new Http(URL)
-                .get(String.format("portal/api/v1/folders/%s/information_systems/%s/environment_prefixes?project_environment_id=%s&reserved=false", ShareData.getString(folder_id), infoSystems, projectEnvId))
-                .assertStatus(200)
-                .jsonPath();
-        List prefix_list = jsonPath.get("list");
-        Random rand = new Random();
-        int i = 0;
-        try {
-            i = rand.nextInt(prefix_list.size());
-        } catch (IllegalArgumentException ex) {
-            ex.printStackTrace();
-            Assert.fail("Количество префиксов <=0");
-        }
-        String prefix = jsonPath.get("list[" + i + "].id");
-        log.info(String.format("Полученный префикс %s", prefix));
-        return prefix;
     }
 
 }
