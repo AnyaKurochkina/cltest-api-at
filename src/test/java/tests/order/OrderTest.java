b6141f5bfe040d5b0814f6250e8a9c3d65f5eaf7
DSMihaylov <h1zzzy66@gmail.com>
2021-07-29 22:32:33 +0300

    delete import

--- src/test/java/tests/order/OrderTest.java
+++ src/test/java/tests/order/OrderTest.java
@@ -4,14 +4,12 @@ import models.orderService.PostgreSQL;
 import models.orderService.interfaces.IProduct;
 import models.orderService.RabbitMq;
 import models.orderService.Rhel;
-import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import steps.Steps;
 import steps.keyCloak.KeyCloakSteps;
 import tests.Tests;
 
@@ -47,8 +45,8 @@ public class OrderTest extends Tests {
 
     static Stream<Arguments> dataProviderMethod() {
         return Stream.of(
-                /*Arguments.arguments(Rhel.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").osVersion("8.latest").build()),
-                Arguments.arguments(Rhel.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").osVersion("7.latest").build()),*/
+                Arguments.arguments(Rhel.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").osVersion("8.latest").build()),
+                Arguments.arguments(Rhel.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").osVersion("7.latest").build()),
                 Arguments.arguments(RabbitMq.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").build()),
                 Arguments.arguments(PostgreSQL.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").osVersion("8.latest").postgresql_version("12").build()),
                 Arguments.arguments(PostgreSQL.builder().env("DEV").segment("dev-srv-app").dataCentre("5").platform("Nutanix").osVersion("8.latest").postgresql_version("11").build())
