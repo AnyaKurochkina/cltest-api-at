d7aa3d6fc699be8b953e5317d6e56bf602ae32b7
Roman Ermakov <rvermakov@dev.vtb.ru>
2021-09-09 13:07:42 +0300

    добавлена проверка уникальности value TmsLink

--- src/test/java/org/junit/TestsExecutionListener.java
+++ src/test/java/org/junit/TestsExecutionListener.java
@@ -1,17 +1,41 @@
 package org.junit;
 
+import io.qameta.allure.TmsLink;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
 import core.CacheService;
 import steps.Steps;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.*;
+
 import static core.helper.Configure.ENV;
 
+@Log4j2
 public class TestsExecutionListener implements TestExecutionListener {
 
     public void testPlanExecutionStarted(TestPlan testPlan) {
+        List<Class<?>> classes = ClassFinder.find("tests");
+        Set<String> values = new HashSet<>();
+        for(Class<?> c : classes){
+            for(Method method : c.getDeclaredMethods()){
+                if(method.isAnnotationPresent(TmsLink.class)){
+                    String value = method.getAnnotation(TmsLink.class).value();
+                    if(values.contains(value)) {
+                        log.error(String.format("Значение '%s' у аннотации TmsLink не уникально", value));
+                        System.exit(1);
+                    }
+                    values.add(value);
+                }
+            }
+        }
         CacheService.loadEntities(Steps.dataFolder + "/shareFolder/" + ((System.getProperty("share") != null) ? System.getProperty("share") : "shareData") + ".json");
     }
 
@@ -22,4 +46,8 @@ public class TestsExecutionListener implements TestExecutionListener {
         fooWriter.write("ENV=" + ENV);
         fooWriter.close();
     }
+
+
+
+
 }
