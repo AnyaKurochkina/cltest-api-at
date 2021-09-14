2803151d61c3461a1ef4aebdfacec0208041d49d
Roman Ermakov <rvermakov@dev.vtb.ru>
2021-09-14 10:26:45 +0300

    методы и тесты на ТП

--- src/test/java/models/tarifficator/TariffPlan.java
+++ src/test/java/models/tarifficator/TariffPlan.java
@@ -1,38 +1,38 @@
 package models.tarifficator;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.GsonBuilder;
 import core.CacheService;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import models.Entity;
 import org.json.JSONObject;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 @Builder
-public class TariffPlan {
-    boolean base;
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class TariffPlan extends Entity {
+    Boolean base;
     String baseTariffPlanId;
-    String beginDate;
-    String createdAt;
-    String endDate;
+    Date beginDate;
+    Date createdAt;
+    Date endDate;
+    @EqualsAndHashCode.Exclude
     String id;
     String oldTariffPlanId;
     String organizationName;
-    String status;
+    TariffPlanStatus status;
     List<TariffClass> tariffClasses;
+    @EqualsAndHashCode.Exclude
     String title;
-    boolean updateOrders;
-    String updatedAt;
+    Boolean updateOrders;
+    Date updatedAt;
 
     public JSONObject serialize() {
         return new JSONObject("{\"tariff_plan\":" + CacheService.toJson(this) + "}");
     }
 
     public static TariffPlan deserialize(String object) {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
-        return gsonBuilder.create().fromJson(object, TariffPlan.class);
+        return CacheService.getCustomGson().fromJson(object, TariffPlan.class);
     }
 }
