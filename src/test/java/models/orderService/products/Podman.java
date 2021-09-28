aefb7ef58ce42df3353449a655470d9067febac6
01morozov92 <01morozov92@gmail.com>
2021-09-28 16:20:50 +0300

    Добавил Podman.java, а так же в тестовые данные стенда PROD, добавил ClickHouse в тестовые данные стенда PROD

---
+++ src/test/java/models/orderService/products/Podman.java
@@ -0,0 +1,89 @@
+package models.orderService.products;
+
+import core.helper.Http;
+import io.restassured.path.json.JsonPath;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import models.authorizer.AccessGroup;
+import models.authorizer.Project;
+import models.orderService.interfaces.IProduct;
+import models.orderService.interfaces.ProductStatus;
+import models.subModels.Flavor;
+import org.json.JSONObject;
+import org.junit.Action;
+import steps.orderService.OrderServiceSteps;
+
+import java.util.List;
+
+@ToString(callSuper = true, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(callSuper = true)
+@Log4j2
+@Data
+public class Podman extends IProduct {
+
+    @ToString.Include
+    String segment;
+    String dataCentre;
+    String domain;
+    @ToString.Include
+    String platform;
+    @ToString.Include
+    String osVersion;
+    Flavor flavor;
+
+    public Podman() {
+        jsonTemplate = "/orders/podman.json";
+        productName = "Podman";
+    }
+
+    @Override
+    public void order() {
+        JSONObject template = getJsonParametrizedTemplate();
+        domain = orderServiceSteps.getDomainBySegment(this, segment);
+        log.info("Отправка запроса на создание заказа для " + productName);
+        JsonPath jsonPath = new Http(OrderServiceSteps.URL)
+                .setProjectId(projectId)
+                .post("order-service/api/v1/projects/" + projectId + "/orders", template)
+                .assertStatus(201)
+                .jsonPath();
+        orderId = jsonPath.get("[0].id");
+        orderServiceSteps.checkOrderStatus("success", this);
+        setStatus(ProductStatus.CREATED);
+        cacheService.saveEntity(this);
+    }
+
+    @Override
+    public JSONObject getJsonParametrizedTemplate() {
+        Project project = cacheService.entity(Project.class)
+                .withField("env", env)
+                .forOrders(true)
+                .getEntity();
+        if(productId == null) {
+            projectId = project.id;
+            productId = orderServiceSteps.getProductId(this);
+        }
+        AccessGroup accessGroup = cacheService.entity(AccessGroup.class)
+                .withField("projectName", project.id)
+                .getEntity();
+        List<Flavor> flavorList = referencesStep.getProductFlavorsLinkedList(this);
+        flavor = flavorList.get(0);
+        return jsonHelper.getJsonTemplate(jsonTemplate)
+                .set("$.order.product_id", productId)
+                .set("$.order.attrs.domain", domain)
+                .set("$.order.attrs.default_nic.net_segment", segment)
+                .set("$.order.attrs.data_center", dataCentre)
+                .set("$.order.attrs.flavor", new JSONObject(flavor.toString()))
+                .set("$.order.attrs.platform", platform)
+                .set("$.order.attrs.ad_logon_grants[0].groups[0]", accessGroup.name)
+                .set("$.order.project_name", project.id)
+                .build();
+    }
+
+    @Override
+    @Action("Удалить рекурсивно")
+    public void delete(String action) {
+        super.delete(action);
+    }
+}
