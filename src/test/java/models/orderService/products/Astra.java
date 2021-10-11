525136de0bf9bf6ddd0aa5a65d74e9b7d77c7710
Roman Ermakov <rvermakov@dev.vtb.ru>
2021-10-11 14:54:24 +0300

    add Astra

---
+++ src/test/java/models/orderService/products/Astra.java
@@ -0,0 +1,89 @@
+package models.orderService.products;
+
+import core.helper.Http;
+import io.restassured.path.json.JsonPath;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import models.authorizer.AccessGroup;
+import models.authorizer.Project;
+import models.orderService.interfaces.IProduct;
+import models.orderService.interfaces.ProductStatus;
+import models.subModels.Flavor;
+import org.json.JSONObject;
+import org.junit.Action;
+import steps.orderService.OrderServiceSteps;
+
+import java.util.List;
+
+@ToString(callSuper = true, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(callSuper = true)
+@Log4j2
+@Data
+public class Astra extends IProduct {
+    @ToString.Include
+    String segment;
+    String dataCentre;
+    @ToString.Include
+    String platform;
+    @ToString.Include
+    String osVersion;
+    String domain;
+    Flavor flavor;
+
+    public Astra() {
+        jsonTemplate = "/orders/astra_general_application.json";
+        productName = "Astra General Application (OS only)";
+    }
+
+    @Override
+    public void order() {
+        JSONObject template = getJsonParametrizedTemplate();
+        domain = orderServiceSteps.getDomainBySegment(this, segment);
+        log.info("Отправка запроса на создание заказа для " + productName);
+        JsonPath array = new Http(OrderServiceSteps.URL)
+                .setProjectId(projectId)
+                .post("order-service/api/v1/projects/" + projectId + "/orders", template)
+                .assertStatus(201)
+                .jsonPath();
+        orderId = array.get("[0].id");
+        orderServiceSteps.checkOrderStatus("success", this);
+        setStatus(ProductStatus.CREATED);
+        cacheService.saveEntity(this);
+    }
+
+    @Override
+    public JSONObject getJsonParametrizedTemplate() {
+        Project project = cacheService.entity(Project.class)
+                .withField("env", env)
+                .forOrders(true)
+                .getEntity();
+        if (productId == null) {
+            projectId = project.id;
+            productId = orderServiceSteps.getProductId(this);
+        }
+        AccessGroup accessGroup = cacheService.entity(AccessGroup.class)
+                .withField("projectName", project.id)
+                .getEntity();
+        List<Flavor> flavorList = referencesStep.getProductFlavorsLinkedList(this);
+        flavor = flavorList.get(0);
+        return jsonHelper.getJsonTemplate(jsonTemplate)
+                .set("$.order.product_id", productId)
+                .set("$.order.attrs.domain", domain)
+                .set("$.order.attrs.flavor", new JSONObject(flavor.toString()))
+                .set("$.order.attrs.default_nic.net_segment", segment)
+                .set("$.order.attrs.data_center", dataCentre)
+                .set("$.order.attrs.platform", platform)
+                .set("$.order.attrs.os_version", osVersion)
+                .set("$.order.attrs.ad_logon_grants[0].groups[0]", accessGroup.name)
+                .set("$.order.project_name", project.id)
+                .build();
+    }
+
+    @Override
+    @Action("Перезагрузить по питанию")
+    public void restart(String action) {
+        super.restart(action);
+    }
+}
