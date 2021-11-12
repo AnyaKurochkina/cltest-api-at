8eb5bde320df13ce6ba9781e79891c8487b27b67
01morozov92 <01morozov92@gmail.com>
2021-11-12 09:44:50 +0300

    some imports

--- src/test/java/steps/productCatalog/ActionsSteps.java
+++ src/test/java/steps/productCatalog/ActionsSteps.java
@@ -3,11 +3,9 @@ package steps.productCatalog;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import core.CacheService;
 import core.helper.Http;
-import io.restassured.response.ValidatableResponse;
 import lombok.SneakyThrows;
 import models.productCatalog.StringModel.StringModelResponse;
 import models.productCatalog.testModel.ActionResponse;
-import models.tarifficator.TariffPlan;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
@@ -16,37 +14,33 @@ public class ActionsSteps {
     @SneakyThrows
     @Test
     public void test() {
-//        String object = new Http("http://d4-product-catalog.apps.d0-oscp.corp.dev.vtb/")
-//                .setContentType("application/json")
-//                .setWithoutToken()
-//                .get("actions")
-//                .assertStatus(200)
-//                .toString();
+        String object = new Http("http://d4-product-catalog.apps.d0-oscp.corp.dev.vtb/")
+                .setContentType("application/json")
+                .setWithoutToken()
+                .get("actions")
+                .assertStatus(200)
+                .toString();
 
-        String object = "{\n" +
-                "  \"service_account\": {\n" +
-                "    \"title\": \"TEST02\",\n" +
-                "    \"policy\": {\n" +
-                "      \"bindings\": [\n" +
-                "        {\n" +
-                "          \"role\": null\n" +
-                "        }\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
+//        String object = "{\n" +
+//                "  \"service_account\": {\n" +
+//                "    \"title\": \"TEST02\",\n" +
+//                "    \"policy\": {\n" +
+//                "      \"bindings\": [\n" +
+//                "        {\n" +
+//                "          \"role\": null\n" +
+//                "        }\n" +
+//                "      ]\n" +
+//                "    }\n" +
+//                "  }\n" +
+//                "}";
 
-        StringModelResponse response = mapResponseOnClass(object, StringModelResponse.class);
-        StringModelResponse responseGson = CacheService.getCustomGson().fromJson(object, StringModelResponse.class);
-
-
+        ActionResponse response = convertResponseOnClass(object, ActionResponse.class);
+//        StringModelResponse responseGson = CacheService.getCustomGson().fromJson(object, StringModelResponse.class);
         System.out.println();
-
-
     }
 
     @SneakyThrows
-    public static <T> T mapResponseOnClass(String rawJson, Class<T> clazz){
+    public static <T> T convertResponseOnClass(String rawJson, Class<T> clazz){
         JSONObject jsonObject = new JSONObject(rawJson);
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.convertValue(jsonObject.toMap(), clazz);
