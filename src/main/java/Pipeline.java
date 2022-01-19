73f3dda4f032ed694cf712565abc355ca9a1e324
VTB4043675 <rvermakov@dev.vtb.ru>
2022-01-19 14:56:55 +0300

    #13

--- src/main/java/Pipeline.java
+++ src/main/java/Pipeline.java
@@ -46,7 +46,7 @@ public class Pipeline {
             }
             String command = "-Dsecret=123456 -Denv=IFT -Dtest=" + String.join(",", externalIds);
             try (PrintWriter writerCommand = new PrintWriter(new BufferedWriter(new FileWriter("run.sh", false)))) {
-                writerCommand.println("mvn clean test" + command);
+                writerCommand.println("mvn clean test " + command);
                 System.out.println("COMMAND_LINE: " + command);
             }
         }
