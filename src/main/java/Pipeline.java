f25de5a09592501d2ecd4f67e82cc930473a0edb
VTB4043675 <rvermakov@dev.vtb.ru>
2022-01-20 11:00:02 +0300

    #18

--- src/main/java/Pipeline.java
+++ src/main/java/Pipeline.java
@@ -45,8 +45,8 @@ public class Pipeline {
                 writer.println(externalId + "=" + result.query("/configuration/id"));
             }
             String command = "-Dsecret=123456 -Denv=IFT -Dtest=" + String.join(",", externalIds);
-//            System.out.println("##teamcity[setParameter name='env.testArguments' value='" + command + "']");
-            System.out.println("##teamcity[setParameter name='env.MAVEN_OPTS' value='" + command + "']");
+            System.out.println("##teamcity[setParameter name='env.testArguments' value='" + command + "']");
+//            System.out.println("##teamcity[setParameter name='env.MAVEN_OPTS' value='" + command + "']");
 //            try (PrintWriter writerCommand = new PrintWriter(new BufferedWriter(new FileWriter("run.sh", false)))) {
 //                writerCommand.println("mvn clean install -DskipTests=false " + command);
 //                System.out.println("COMMAND_LINE: " + command);
