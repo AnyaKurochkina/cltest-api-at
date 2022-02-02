63a4d4962fc6504fd95e39b87e5ba7e7ad1ef931
01morozov92 <01morozov92@gmail.com>
2022-02-02 19:43:15 +0300

    добавил новый эластик

---
+++ src/test/java/tests/orderService/ElasticsearchOpensearchClusterTest.java
@@ -0,0 +1,87 @@
+package tests.orderService;
+
+import core.helper.MarkDelete;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import models.orderService.interfaces.ProductStatus;
+import models.orderService.products.Elasticsearch;
+import models.orderService.products.ElasticsearchOpensearchCluster;
+import org.junit.ProductArgumentsProvider;
+import org.junit.Source;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import tests.Tests;
+
+@Epic("Продукты")
+@Feature("Elasticsearch Opensearch Cluster")
+@Tags({@Tag("regress"), @Tag("orders"), @Tag("elasticsearch_opensearch_cluster"), @Tag("prod")})
+public class ElasticsearchOpensearchClusterTest extends Tests {
+
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Создать {0}")
+    void create(ElasticsearchOpensearchCluster product) {
+        //noinspection EmptyTryBlock
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {}
+    }
+
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Проверить конфигурацию {0}")
+    void refreshVmConfig(ElasticsearchOpensearchCluster product) {
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {
+            elastic.checkPreconditionStatusProduct(ProductStatus.CREATED);
+            elastic.refreshVmConfig();
+        }
+    }
+
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Включить {0}")
+    void start(ElasticsearchOpensearchCluster product) {
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {
+            elastic.checkPreconditionStatusProduct(ProductStatus.CREATED);
+            elastic.stopHard();
+            elastic.start();
+
+        }
+    }
+
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Выключить {0}")
+    void stopSoft(ElasticsearchOpensearchCluster product) {
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {
+            elastic.stopSoft();
+            elastic.start();
+        }
+    }
+
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Выключить принудительно {0}")
+    void stopHard(ElasticsearchOpensearchCluster product) {
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {
+            elastic.stopHard();
+            elastic.start();
+        }
+    }
+
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Перезагрузить по питанию {0}")
+    void restart(ElasticsearchOpensearchCluster product) {
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {
+            elastic.restart();
+        }
+    }
+
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Удалить {0}")
+    @MarkDelete
+    void delete(ElasticsearchOpensearchCluster product) {
+        try (ElasticsearchOpensearchCluster elastic = product.createObjectExclusiveAccess()) {
+            elastic.deleteObject();
+        }
+    }
+}
