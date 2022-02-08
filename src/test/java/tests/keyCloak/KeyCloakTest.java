df3a6b18d7a031166f2e26abed515e5ce6f8a978
VTB4043675 <rvermakov@dev.vtb.ru>
2022-02-08 17:24:51 +0300

    #68

--- src/test/java/tests/keyCloak/KeyCloakTest.java
+++ src/test/java/tests/keyCloak/KeyCloakTest.java
@@ -4,6 +4,8 @@ import core.enums.Role;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
+import models.authorizer.Project;
+import models.authorizer.ServiceAccount;
 import org.junit.jupiter.api.*;
 import steps.keyCloak.KeyCloakSteps;
 import tests.Tests;
@@ -19,5 +21,12 @@ public class KeyCloakTest extends Tests {
     @DisplayName("Получение токена")
     void getToken() {
         KeyCloakSteps.getUserToken(Role.ADMIN);
+    }
+
+    @Test
+    @TmsLink("111259")
+    @DisplayName("Получение токена")
+    void getTokenServiceAccount() {
+        KeyCloakSteps.getServiceAccountToken(((Project)Project.builder().isForOrders(true).build().createObject()).getId());
     }
 }
