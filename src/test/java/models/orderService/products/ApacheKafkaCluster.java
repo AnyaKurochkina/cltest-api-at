f4e635de0e7eac5c53831742b3aaa7acad4099d2
VTB4043675 <rvermakov@dev.vtb.ru>
2022-02-22 15:37:28 +0300

    win dev kafka

--- src/test/java/models/orderService/products/ApacheKafkaCluster.java
+++ src/test/java/models/orderService/products/ApacheKafkaCluster.java
@@ -80,7 +80,7 @@ public class ApacheKafkaCluster extends IProduct {
                 .set("$.order.attrs.ad_logon_grants[0].groups[0]", accessGroup.getPrefixName())
                 .set("$.order.project_name", project.id)
                 .set("$.order.attrs.os_version", osVersion)
-                .set("$.order.attrs.on_support", getSupport())
+                .set("$.order.attrs.on_support", project.getProjectEnvironment().getEnvType().contains("TEST"))
                 .set("$.order.label", getLabel())
                 .build();
     }
