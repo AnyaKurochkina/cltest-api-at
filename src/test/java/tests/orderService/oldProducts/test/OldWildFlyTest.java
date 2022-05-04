9a459aca93c06ced4ff9f9f9ffdab6deaf960944
01morozov92 <01morozov92@gmail.com>
2022-05-04 18:49:34 +0300

    add new old tests

--- src/test/java/tests/orderService/oldProducts/test/OldWildFlyTest.java
+++ src/test/java/tests/orderService/oldProducts/test/OldWildFlyTest.java
@@ -2,6 +2,7 @@ package tests.orderService.oldProducts.test;
 
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
+import io.qameta.allure.TmsLink;
 import models.orderService.interfaces.ProductStatus;
 import models.orderService.products.WildFly;
 import org.junit.jupiter.api.*;
@@ -11,21 +12,22 @@ import static models.orderService.interfaces.ProductStatus.STARTED;
 import static models.orderService.interfaces.ProductStatus.STOPPED;
 
 @Epic("Старые продукты TEST")
-@Feature("WildFly OLD")
-@Tags({@Tag("regress"), @Tag("orders"), @Tag("old_wildfly"), @Tag("prod"), @Tag("old")})
+@Feature("WildFly Astra OLD")
+@Tags({@Tag("regress"), @Tag("orders"), @Tag("old_wildfly_astra"), @Tag("prod"), @Tag("old")})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OldWildFlyTest extends Tests {
 
     final WildFly wildFly = WildFly.builder()
             .projectId("proj-juh8ynkvtn")
-            .productId("972a66f1-cd45-437f-b920-676bad68e594")
-            .orderId("05e157b1-b9ae-4b23-9e16-74d5e6071db4")
-            .productName("WildFly")
+            .productId("9c777632-052c-4eb8-b176-e5e3375689e4")
+            .orderId("d73d8b8c-255e-4679-9da6-55670e610ec1")
+            .productName("Разработка WildFly Astra")
             .build();
 
     @Order(1)
-    @DisplayName("Расширить WildFly OLD")
+    @TmsLink("841689")
+    @DisplayName("Расширить WildFly Astra OLD")
     @Test
     void expandMountPoint() {
         if (wildFly.productStatusIs(STOPPED)) {
@@ -35,7 +37,8 @@ public class OldWildFlyTest extends Tests {
     }
 
     @Order(2)
-    @DisplayName("Перезагрузить WildFly OLD")
+    @TmsLink("841694")
+    @DisplayName("Перезагрузить WildFly Astra OLD")
     @Test
     void restart() {
         if (wildFly.productStatusIs(STOPPED)) {
@@ -45,17 +48,8 @@ public class OldWildFlyTest extends Tests {
     }
 
     @Order(3)
-    @DisplayName("Выключить WildFly OLD")
-    @Test
-    void stopSoft() {
-        if (wildFly.productStatusIs(STOPPED)) {
-            wildFly.start();
-        }
-        wildFly.stopSoft();
-    }
-
-    @Order(4)
-    @DisplayName("Изменить конфигурацию WildFly OLD")
+    @TmsLink("841690")
+    @DisplayName("Изменить конфигурацию WildFly Astra OLD")
     @Test
     void resize() {
         if (wildFly.productStatusIs(STARTED)) {
@@ -65,8 +59,20 @@ public class OldWildFlyTest extends Tests {
         wildFly.resize(wildFly.getMinFlavor());
     }
 
+    @Order(4)
+    @TmsLink("841688")
+    @DisplayName("Проверить конфигурацию WildFly Astra OLD")
+    @Test
+    void refreshVmConfig() {
+        if (wildFly.productStatusIs(STOPPED)) {
+            wildFly.start();
+        }
+        wildFly.refreshVmConfig();
+    }
+
     @Order(5)
-    @DisplayName("Включить WildFly OLD")
+    @TmsLink("841693")
+    @DisplayName("Включить WildFly Astra OLD")
     @Test
     void start() {
         if (wildFly.productStatusIs(STARTED)) {
@@ -76,7 +82,8 @@ public class OldWildFlyTest extends Tests {
     }
 
     @Order(6)
-    @DisplayName("Обновить сертификаты WildFly OLD")
+    @TmsLink("841692")
+    @DisplayName("Обновить сертификаты WildFly Astra OLD")
     @Test
     void updateCerts() {
         if (wildFly.productStatusIs(STOPPED)) {
@@ -86,12 +93,24 @@ public class OldWildFlyTest extends Tests {
     }
 
     @Order(7)
-    @DisplayName("Выключить принудительно WildFly OLD")
+    @TmsLink("841697")
+    @DisplayName("Выключить принудительно WildFly Astra OLD")
     @Test
     void stopHard() {
         if (wildFly.productStatusIs(STOPPED)) {
             wildFly.start();
         }
         wildFly.stopHard();
+    }
+
+    @Order(8)
+    @TmsLink("841696")
+    @DisplayName("Выключить WildFly Astra OLD")
+    @Test
+    void stopSoft() {
+        if (wildFly.productStatusIs(STOPPED)) {
+            wildFly.start();
+        }
+        wildFly.stopSoft();
     }
 }
