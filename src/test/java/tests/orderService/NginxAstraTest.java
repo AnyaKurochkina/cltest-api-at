a8e5d68ad0321451a71ec3f1070f99ba948b7665
VTB4043675 <rvermakov@dev.vtb.ru>
2022-05-06 12:27:41 +0300

    add Nginx astra

---
+++ src/test/java/tests/orderService/NginxAstraTest.java
@@ -0,0 +1,115 @@
+package tests.orderService;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.TmsLink;
+import models.orderService.products.Nginx;
+import org.junit.MarkDelete;
+import org.junit.ProductArgumentsProvider;
+import org.junit.Source;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import tests.Tests;
+
+@Epic("Продукты")
+@Feature("Nginx")
+@Tags({@Tag("regress"), @Tag("orders"), @Tag("nginx"), @Tag("prod")})
+public class NginxAstraTest extends Tests {
+    final String productName = "Nginx Astra";
+
+    @TmsLink("846594")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Создать {0}")
+    void create(Nginx product) {
+        product.setProductName(productName);
+        //noinspection EmptyTryBlock
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {}
+    }
+
+    @TmsLink("846597")
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Расширить {0}")
+    void expandMountPoint(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.expandMountPoint();
+        }
+    }
+
+    @TmsLink("846600")
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Перезагрузить {0}")
+    void restart(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.restart();
+        }
+    }
+
+    @TmsLink("846599")
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Выключить {0}")
+    void stopSoft(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.stopSoft();
+            nginx.start();
+        }
+    }
+
+    @TmsLink("846596")
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Изменить конфигурацию {0}")
+    void resize(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.stopHard();
+            try {
+                nginx.resize(nginx.getMaxFlavor());
+            } finally {
+                nginx.start();
+            }
+
+        }
+    }
+
+    @TmsLink("846598")
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Включить {0}")
+    void start(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.stopHard();
+            nginx.start();
+        }
+    }
+
+    @TmsLink("846595")
+    @Tag("actions")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Выключить принудительно {0}")
+    void stopHard(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.stopHard();
+            nginx.start();
+        }
+    }
+
+    @TmsLink("846593")
+    @Source(ProductArgumentsProvider.PRODUCTS)
+    @ParameterizedTest(name = "Удалить {0}")
+    @MarkDelete
+    void delete(Nginx product) {
+        product.setProductName(productName);
+        try (Nginx nginx = product.createObjectExclusiveAccess()) {
+            nginx.deleteObject();
+        }
+    }
+}
