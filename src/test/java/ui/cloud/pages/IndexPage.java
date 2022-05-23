663556ff6f8ee8b5c15b167105cf72ac858a8ad9
VTB4043675 <rvermakov@dev.vtb.ru>
2022-05-23 12:53:45 +0300

    test #29

--- src/test/java/ui/cloud/pages/IndexPage.java
+++ src/test/java/ui/cloud/pages/IndexPage.java
@@ -6,7 +6,7 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class IndexPage {
-    SelenideElement orderMoreBtn = $x("//button[contains(., 'Заказать еще')]");
+    SelenideElement orderMoreBtn = $x("//button[contains(., 'Заказать еще3')]");
 
     public IndexPage() {
         orderMoreBtn.shouldBe(Condition.visible).shouldBe(Condition.enabled);
