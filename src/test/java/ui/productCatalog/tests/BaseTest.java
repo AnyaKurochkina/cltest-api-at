1880276cb4098b35cc079cdbe2cea3cd69ae0227
airat.muzafarov <inkin_88@mail.ru>
2022-05-27 14:54:49 +0300

    refactoring

--- src/test/java/ui/productCatalog/tests/BaseTest.java
+++ src/test/java/ui/productCatalog/tests/BaseTest.java
@@ -1,10 +1,6 @@
 package ui.productCatalog.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
@@ -20,20 +16,20 @@ abstract public class BaseTest extends Tests {
     private static final String login = getAppProp("user.login");
     private static final String password = getAppProp("user.password");
 
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
-        Configuration.driverManagerEnabled = true;
-        Configuration.browserSize = "1366x968";
-        Configuration.headless = false;
-        Configuration.timeout = 20000;
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
-    }
+//    public void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        Configuration.browser = "chrome";
+//        Configuration.driverManagerEnabled = true;
+//        Configuration.browserSize = "1366x968";
+//        Configuration.headless = false;
+//        Configuration.timeout = 20000;
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+//    }
 
     @BeforeEach
     public void init() {
-        setUp();
-        login();
+     //   setUp();
+        new LoginPage().singIn();
     }
 
     @AfterEach
@@ -43,6 +39,6 @@ abstract public class BaseTest extends Tests {
 
     public void login() {
         LoginPage loginPage = new LoginPage();
-        loginPage.login(login, password);
+        loginPage.singIn();
     }
 }
