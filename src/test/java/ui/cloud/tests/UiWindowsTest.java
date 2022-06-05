f79dbcbebf08e31e6e055d4fb1c2fa09cf83d940
vtb4055503 <a.panteleev@dev.vtb.ru>
2022-06-05 11:31:21 +0300

    add methods and checks

--- src/test/java/ui/cloud/tests/UiWindowsTest.java
+++ src/test/java/ui/cloud/tests/UiWindowsTest.java
@@ -22,8 +22,13 @@ import ui.uiExtesions.InterceptTestExtension;
 @Tags({@Tag("ui_windows")})
 @Log4j2
 public class UiWindowsTest extends Tests {
+
     Windows product;
+
+    private static final String READY_ORDER_URL = "https://prod-portal-front.cloud.vtb.ru/vm/orders/c749f2f7-e6eb-4c31-94c5-34cd5cd2f091/main?context=proj-evw9xv5qao&type=project&org=vtb";
+
     CommonChecks commonChecks = new CommonChecks();
+
     //TODO: пока так :)
     public UiWindowsTest() {
         if (Configure.ENV.equals("prod"))
@@ -36,66 +41,68 @@ public class UiWindowsTest extends Tests {
     @BeforeEach
     @Title("Авторизация на портале")
     void beforeEach() {
-        new LoginPage(product.getProjectId())
+//        new LoginPage(product.getProjectId())
+//                .singIn();
+        new LoginPage(READY_ORDER_URL)
                 .singIn();
     }
 
-    @Test
-    @TmsLink("872651")
-    @Order(1)
-    @DisplayName("UI Windows. Заказ")
-    void orderWindows() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        WindowsOrderPage orderPage = new WindowsOrderPage();
-//        //Проверки полей до заказа
-//        commonChecks.getMark().clear();
-//        commonChecks.getOrderProduct().shouldBe(Condition.disabled);
-//        log.info("Проверка кнопки \"Заказать\" до заполнения полей");
-//        commonChecks.getOrderPricePerDay().getAttribute("textContent").contains("— ₽");
-//        log.info("Проверка нельзя заказать до заполнения полей");
-//
-//        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "0", "10");
-//        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "100", "30");
-//        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "N", "10");
-//        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "1", "1");
-//        log.info("Проверки поля количества VM");
-//
-//
-//        commonChecks.getMark().sendKeys(CTRL+A);
-//        commonChecks.getMark().shouldBe(Condition.matchText("Поле должно содержать от 3 до 64 символов"));
-//        log.info("Проверка кнопки \"Метка\" Поле должно содержать от 3 до 64 символов");
-//        commonChecks.getOrderProduct().shouldBe(Condition.disabled);
-        orderPage.getOsVersion().select(product.getOsVersion());
-        orderPage.getSegment().selectByValue(product.getSegment());
-        orderPage.getPlatform().selectByValue(product.getPlatform());
-        orderPage.getRoleServer().selectByValue(product.getRole());
-        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
-        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
-        orderPage.getGroup().select(accessGroup.getPrefixName());
-//        //пользователь проверяет, что у элемента "Стоимость в сутки" атрибут "textContent" содержит значение "≈"
-//        commonChecks.isCostDayContains("≈");
-//        //пользователь проверяет детали заказа
-//        commonChecks.checkOrderDetails(commonChecks.getCalculationDetails(), "Windows Server");
-        orderPage.orderClick();
-        new ProductsPage()
-                .getRowByColumn("Продукт",
-                        orderPage.getLabel())
-                .hover()
-                .click();
-        WindowsPage winPage = new WindowsPage(product);
-        winPage.waitChangeStatus();
-        winPage.checkLastAction();
-//        // Проверки после заказа продукта
-//        commonChecks.checkHeaderHistoryTable();
-//        commonChecks.checkHistoryRowDeployOk();
-//        commonChecks.checkHistoryRowDeployErr();
-//        commonChecks.getHistoryRow0().getAttribute("title").contains("Просмотр схемы выполнения");
-//        log.info("пользователь проверяет, что на странице присутствует текст \"Просмотр схемы выполнения\"");
-//        commonChecks.getGraphScheme().shouldBe(Condition.visible);
-//        log.info(" пользователь проверяет наличие элемента \"Схема выполнения\"");
-    }
+//    @Test
+//    @TmsLink("872651")
+//    @Order(1)
+//    @DisplayName("UI Windows. Заказ")
+//    void orderWindows() {
+//        new IndexPage()
+//                .clickOrderMore()
+//                .selectProduct(product.getProductName());
+//        WindowsOrderPage orderPage = new WindowsOrderPage();
+////        //Проверки полей до заказа
+////        commonChecks.getMark().clear();
+////        commonChecks.getOrderProduct().shouldBe(Condition.disabled);
+////        log.info("Проверка кнопки \"Заказать\" до заполнения полей");
+////        commonChecks.getOrderPricePerDay().getAttribute("textContent").contains("— ₽");
+////        log.info("Проверка нельзя заказать до заполнения полей");
+////
+////        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "0", "10");
+////        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "100", "30");
+////        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "N", "10");
+////        commonChecks.autoChangeableFieldCheck(commonChecks.getVmNumber(), "1", "1");
+////        log.info("Проверки поля количества VM");
+////
+////
+////        commonChecks.getMark().sendKeys(CTRL+A);
+////        commonChecks.getMark().shouldBe(Condition.matchText("Поле должно содержать от 3 до 64 символов"));
+////        log.info("Проверка кнопки \"Метка\" Поле должно содержать от 3 до 64 символов");
+////        commonChecks.getOrderProduct().shouldBe(Condition.disabled);
+//        orderPage.getOsVersion().select(product.getOsVersion());
+//        orderPage.getSegment().selectByValue(product.getSegment());
+//        orderPage.getPlatform().selectByValue(product.getPlatform());
+//        orderPage.getRoleServer().selectByValue(product.getRole());
+//        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
+//        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
+//        orderPage.getGroup().select(accessGroup.getPrefixName());
+////        //пользователь проверяет, что у элемента "Стоимость в сутки" атрибут "textContent" содержит значение "≈"
+////        commonChecks.isCostDayContains("≈");
+////        //пользователь проверяет детали заказа
+////        commonChecks.checkOrderDetails(commonChecks.getCalculationDetails(), "Windows Server");
+//        orderPage.orderClick();
+//        new ProductsPage()
+//                .getRowByColumn("Продукт",
+//                        orderPage.getLabel())
+//                .hover()
+//                .click();
+//        WindowsPage winPage = new WindowsPage(product);
+//        winPage.waitChangeStatus();
+//        winPage.checkLastAction();
+////        // Проверки после заказа продукта
+////        commonChecks.checkHeaderHistoryTable();
+////        commonChecks.checkHistoryRowDeployOk();
+////        commonChecks.checkHistoryRowDeployErr();
+////        commonChecks.getHistoryRow0().getAttribute("title").contains("Просмотр схемы выполнения");
+////        log.info("пользователь проверяет, что на странице присутствует текст \"Просмотр схемы выполнения\"");
+////        commonChecks.getGraphScheme().shouldBe(Condition.visible);
+////        log.info(" пользователь проверяет наличие элемента \"Схема выполнения\"");
+//    }
 
     @Test
     @Order(2)
