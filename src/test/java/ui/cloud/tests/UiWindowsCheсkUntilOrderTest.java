45fda2ffadefaaedbbdb2a3d619d28792b133a3f
vtb4055503 <a.panteleev@dev.vtb.ru>
2022-06-16 20:13:19 +0300

    add methods and checks

---
+++ src/test/java/ui/cloud/tests/UiWindowsCheсkUntilOrderTest.java
@@ -0,0 +1,139 @@
+package ui.cloud.tests;
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import core.helper.Configure;
+import io.qameta.allure.TmsLink;
+import io.qameta.allure.TmsLinks;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import models.orderService.products.Windows;
+import models.portalBack.AccessGroup;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.testit.annotations.Title;
+import tests.Tests;
+import ui.cloud.pages.*;
+import ui.uiExtesions.ConfigExtension;
+import ui.uiExtesions.InterceptTestExtension;
+
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.Keys.BACK_SPACE;
+import static org.openqa.selenium.Keys.CONTROL;
+
+
+@ExtendWith(InterceptTestExtension.class)
+@ExtendWith(ConfigExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Tags({@Tag("ui_windows")})
+@Log4j2
+
+class UiWindowsCheckUntilOrderTest extends Tests {
+    Windows product;
+
+    //TODO: пока так :)
+    public UiWindowsCheckUntilOrderTest() {
+        if (Configure.ENV.equals("prod"))
+            product = Windows.builder().env("DEV").platform("OpenStack").segment("dev-srv-app").build();
+            //product = Windows.builder().env("DEV").platform("OpenStack").segment("dev-srv-app").link("https://prod-portal-front.cloud.vtb.ru/vm/orders/8a37e2a0-1b2c-449e-9f7c-9dfe037a5f03/main?context=proj-frybyv41jh&type=project&org=vtb").build();
+        else
+            product = Windows.builder().env("DSO").platform("vSphere").segment("dev-srv-app").build();
+        product.init();
+
+    }
+
+    @BeforeEach
+    @Title("Авторизация на портале")
+    void beforeEach() {
+        Configuration.browserSize = "1366x768";
+        new LoginPage(product.getProjectId())
+                .singIn();
+    }
+
+    @Test
+    @TmsLink("975914")
+    @Order(1)
+    @DisplayName("UI Windows. Проверка поля Количество VM до заказа продукта")
+    void checkFieldVmNumber() {
+        WindowsPage winPage = new WindowsPage(product);
+        winPage.checkFieldVmNumber();
+    }
+
+    @Test
+    @TmsLink("976622")
+    @Order(2)
+    @DisplayName("UI Windows. Проверка поля Метка до заказа продукта")
+    void checkFieldMark() {
+        WindowsPage winPage = new WindowsPage(product);
+        winPage.getMark().sendKeys(CONTROL + "a");
+        winPage.getMark().sendKeys(BACK_SPACE);
+        $(byText("Поле должно содержать от 3 до 64 символов")).should(Condition.exist);
+        log.info("Проверка поля метка должно содержать от 3 до 64 символов");
+    }
+
+    @Test
+    @TmsLink("976626")
+    @Order(3)
+    @DisplayName("UI Windows. Проверка кнопки Заказать на неактивность, до заполнения полей")
+    void checkBtnOrderDisabled() {
+        WindowsPage winPage = new WindowsPage(product);
+        winPage.getOrderProduct().shouldBe(Condition.disabled);
+    }
+
+    @Test
+    @TmsLink("976629")
+    @Order(4)
+    @DisplayName("UI Windows. Проверка атрибута \"textContent\" на содержание символа \"— ₽\"")
+    void checkFieldAtrTextContentSymbol() {
+        WindowsPage winPage = new WindowsPage(product);
+        new IndexPage()
+                .clickOrderMore()
+                .selectProduct(product.getProductName());
+        Objects.requireNonNull(winPage.getOrderPricePerDay().getAttribute("textContent")).contains("— ₽");
+    }
+
+    @Test
+    @TmsLink("976722")
+    @Order(5)
+    @DisplayName("UI Windows. Проверка у элемента \"Стоимость в сутки\" атрибут \"textContent\" содержит значение \"≈\"")
+    void checkElementAtrTextContentSymbol() {
+        WindowsPage winPage = new WindowsPage(product);
+        new IndexPage()
+                .clickOrderMore()
+                .selectProduct(product.getProductName());
+        WindowsOrderPage orderPage = new WindowsOrderPage();
+        orderPage.getOsVersion().select(product.getOsVersion());
+        orderPage.getSegment().selectByValue(product.getSegment());
+        orderPage.getPlatform().selectByValue(product.getPlatform());
+        orderPage.getRoleServer().selectByValue(product.getRole());
+        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
+        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
+        orderPage.getGroup().select(accessGroup.getPrefixName());
+        winPage.isCostDayContains("≈");
+    }
+
+    @Test
+    @TmsLink("976724")
+    @Order(6)
+    @DisplayName("UI Windows. Проверка Детали заказа.")
+    void checkDetailsOrder() {
+        WindowsPage winPage = new WindowsPage(product);
+        new IndexPage()
+                .clickOrderMore()
+                .selectProduct(product.getProductName());
+        WindowsOrderPage orderPage = new WindowsOrderPage();
+        orderPage.getOsVersion().select(product.getOsVersion());
+        orderPage.getSegment().selectByValue(product.getSegment());
+        orderPage.getPlatform().selectByValue(product.getPlatform());
+        orderPage.getRoleServer().selectByValue(product.getRole());
+        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
+        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
+        orderPage.getGroup().select(accessGroup.getPrefixName());
+        winPage.checkOrderDetails(winPage.getCalculationDetails(), product.getProductName());
+    }
+}
--- src/test/java/ui/cloud/tests/UiWindowsTest.java
+++ src/test/java/ui/cloud/tests/UiWindowsTest.java
@@ -36,19 +36,16 @@ import static org.openqa.selenium.Keys.CONTROL;
 public class UiWindowsTest extends Tests {
 
     Windows product;
-    IProductPage iProductPage = new IProductPage() {
-    };
-    Double prePriceOrderDbl;
-    Double priceOrderDbl;
-    //  String labelOrder = "a-9707-05f0c2f751fc";
+    double prePriceOrderDbl;
+    double priceOrderDbl;
     double costAfterChange;
     double currentCost;
 
     //TODO: пока так :)
     public UiWindowsTest() {
         if (Configure.ENV.equals("prod"))
-            // product = Windows.builder().env("DEV").platform("OpenStack").segment("dev-srv-app").build();
-            product = Windows.builder().env("DEV").platform("OpenStack").segment("dev-srv-app").link("https://prod-portal-front.cloud.vtb.ru/vm/orders/8a37e2a0-1b2c-449e-9f7c-9dfe037a5f03/main?context=proj-frybyv41jh&type=project&org=vtb").build();
+            product = Windows.builder().env("DEV").platform("OpenStack").segment("dev-srv-app").build();
+            //product = Windows.builder().env("DEV").platform("OpenStack").segment("dev-srv-app").link("https://prod-portal-front.cloud.vtb.ru/vm/orders/8a37e2a0-1b2c-449e-9f7c-9dfe037a5f03/main?context=proj-frybyv41jh&type=project&org=vtb").build();
         else
             product = Windows.builder().env("DSO").platform("vSphere").segment("dev-srv-app").build();
         product.init();
@@ -63,96 +60,94 @@ public class UiWindowsTest extends Tests {
                 .singIn();
     }
 
-    @Test
-    @TmsLink("975914")
-    @Order(1)
-    @DisplayName("UI Windows. Проверка поля Количество VM до заказа продукта")
-    void checkFieldVmNumber() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        iProductPage.checkFieldVmNumber();
-    }
-
-    @Test
-    @TmsLink("976622")
-    @Order(2)
-    @DisplayName("UI Windows. Проверка поля Метка до заказа продукта")
-    void checkFieldMark() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        iProductPage.getMark().sendKeys(CONTROL + "a");
-        iProductPage.getMark().sendKeys(BACK_SPACE);
-        $(byText("Поле должно содержать от 3 до 64 символов")).should(Condition.exist);
-        log.info("Проверка поля метка должно содержать от 3 до 64 символов");
-    }
-
-    @Test
-    @TmsLink("976626")
-    @Order(3)
-    @DisplayName("UI Windows. Проверка кнопки Заказать на неактивность, до заполнения полей")
-    void checkBtnOrderDisabled() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        iProductPage.getOrderProduct().shouldBe(Condition.disabled);
-    }
-
-    @Test
-    @TmsLink("976629")
-    @Order(4)
-    @DisplayName("UI Windows. Проверка атрибута \"textContent\" на содержание символа \"— ₽\"")
-    void checkFieldAtrTextContentSymbol() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        Objects.requireNonNull(iProductPage.getOrderPricePerDay().getAttribute("textContent")).contains("— ₽");
-    }
-
-    @Test
-    @TmsLink("976722")
-    @Order(5)
-    @DisplayName("UI Windows. Проверка у элемента \"Стоимость в сутки\" атрибут \"textContent\" содержит значение \"≈\"")
-    void checkElementAtrTextContentSymbol() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        WindowsOrderPage orderPage = new WindowsOrderPage();
-        orderPage.getOsVersion().select(product.getOsVersion());
-        orderPage.getSegment().selectByValue(product.getSegment());
-        orderPage.getPlatform().selectByValue(product.getPlatform());
-        orderPage.getRoleServer().selectByValue(product.getRole());
-        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
-        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
-        orderPage.getGroup().select(accessGroup.getPrefixName());
-        iProductPage.isCostDayContains("≈");
-    }
-
-    @Test
-    @TmsLink("976724")
-    @Order(6)
-    @DisplayName("UI Windows. Проверка Детали заказа.")
-    void checkDetailsOrder() {
-        new IndexPage()
-                .clickOrderMore()
-                .selectProduct(product.getProductName());
-        WindowsOrderPage orderPage = new WindowsOrderPage();
-        orderPage.getOsVersion().select(product.getOsVersion());
-        orderPage.getSegment().selectByValue(product.getSegment());
-        orderPage.getPlatform().selectByValue(product.getPlatform());
-        orderPage.getRoleServer().selectByValue(product.getRole());
-        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
-        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
-        orderPage.getGroup().select(accessGroup.getPrefixName());
-        iProductPage.checkOrderDetails(iProductPage.getCalculationDetails(), product.getProductName());
-    }
+//    @Test
+//    @TmsLink("975914")
+//    @Order(1)
+//    @DisplayName("UI Windows. Проверка поля Количество VM до заказа продукта")
+//    void checkFieldVmNumber() {
+//        WindowsPage winPage = new WindowsPage(product);
+//        winPage.checkFieldVmNumber();
+//    }
+//
+//    @Test
+//    @TmsLink("976622")
+//    @Order(2)
+//    @DisplayName("UI Windows. Проверка поля Метка до заказа продукта")
+//    void checkFieldMark() {
+//        WindowsPage winPage = new WindowsPage(product);
+//        winPage.getMark().sendKeys(CONTROL + "a");
+//        winPage.getMark().sendKeys(BACK_SPACE);
+//        $(byText("Поле должно содержать от 3 до 64 символов")).should(Condition.exist);
+//        log.info("Проверка поля метка должно содержать от 3 до 64 символов");
+//    }
+//
+//    @Test
+//    @TmsLink("976626")
+//    @Order(3)
+//    @DisplayName("UI Windows. Проверка кнопки Заказать на неактивность, до заполнения полей")
+//    void checkBtnOrderDisabled() {
+//        WindowsPage winPage = new WindowsPage(product);
+//        winPage.getOrderProduct().shouldBe(Condition.disabled);
+//    }
+//
+//    @Test
+//    @TmsLink("976629")
+//    @Order(4)
+//    @DisplayName("UI Windows. Проверка атрибута \"textContent\" на содержание символа \"— ₽\"")
+//    void checkFieldAtrTextContentSymbol() {
+//        WindowsPage winPage = new WindowsPage(product);
+//        new IndexPage()
+//                .clickOrderMore()
+//                .selectProduct(product.getProductName());
+//        Objects.requireNonNull(winPage.getOrderPricePerDay().getAttribute("textContent")).contains("— ₽");
+//    }
+//
+//    @Test
+//    @TmsLink("976722")
+//    @Order(5)
+//    @DisplayName("UI Windows. Проверка у элемента \"Стоимость в сутки\" атрибут \"textContent\" содержит значение \"≈\"")
+//    void checkElementAtrTextContentSymbol() {
+//        WindowsPage winPage = new WindowsPage(product);
+//        new IndexPage()
+//                .clickOrderMore()
+//                .selectProduct(product.getProductName());
+//        WindowsOrderPage orderPage = new WindowsOrderPage();
+//        orderPage.getOsVersion().select(product.getOsVersion());
+//        orderPage.getSegment().selectByValue(product.getSegment());
+//        orderPage.getPlatform().selectByValue(product.getPlatform());
+//        orderPage.getRoleServer().selectByValue(product.getRole());
+//        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
+//        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
+//        orderPage.getGroup().select(accessGroup.getPrefixName());
+//        winPage.isCostDayContains("≈");
+//    }
+//
+//    @Test
+//    @TmsLink("976724")
+//    @Order(6)
+//    @DisplayName("UI Windows. Проверка Детали заказа.")
+//    void checkDetailsOrder() {
+//        WindowsPage winPage = new WindowsPage(product);
+//        new IndexPage()
+//                .clickOrderMore()
+//                .selectProduct(product.getProductName());
+//        WindowsOrderPage orderPage = new WindowsOrderPage();
+//        orderPage.getOsVersion().select(product.getOsVersion());
+//        orderPage.getSegment().selectByValue(product.getSegment());
+//        orderPage.getPlatform().selectByValue(product.getPlatform());
+//        orderPage.getRoleServer().selectByValue(product.getRole());
+//        orderPage.getConfigure().selectByValue(Product.getFlavor(product.getMinFlavor()));
+//        AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
+//        orderPage.getGroup().select(accessGroup.getPrefixName());
+//        winPage.checkOrderDetails(winPage.getCalculationDetails(), product.getProductName());
+//    }
 
     @Test
     @TmsLink("872651")
     @Order(7)
     @DisplayName("UI Windows. Заказ")
     void orderWindows() {
+        WindowsPage winPage = new WindowsPage(product);
         new IndexPage()
                 .clickOrderMore()
                 .selectProduct(product.getProductName());
@@ -165,17 +160,16 @@ public class UiWindowsTest extends Tests {
         AccessGroup accessGroup = AccessGroup.builder().projectName(product.getProjectId()).build().createObject();
         orderPage.getGroup().select(accessGroup.getPrefixName());
         //получает стоимосить на предбиллинге
-        iProductPage.getOrderBtn().shouldBe(Condition.visible);
-        iProductPage.getLoadOrderPricePerDay().shouldBe(Condition.enabled);
-        iProductPage.getLoadOrderPricePerDay().shouldBe(Condition.disappear);
-        prePriceOrderDbl = iProductPage.convertToDblPriceOrder(iProductPage.getOrderPricePerDay().getAttribute("textContent"));
+        winPage.getOrderBtn().shouldBe(Condition.visible);
+        winPage.getLoadOrderPricePerDay().shouldBe(Condition.enabled);
+        winPage.getLoadOrderPricePerDay().shouldBe(Condition.disappear);
+        prePriceOrderDbl = winPage.convertToDblPriceOrder(winPage.getOrderPricePerDay().getAttribute("textContent"));
         orderPage.orderClick();
         new ProductsPage()
                 .getRowByColumn("Продукт",
                         orderPage.getLabel())
                 .hover()
                 .click();
-        WindowsPage winPage = new WindowsPage(product);
         winPage.waitChangeStatus();
         winPage.checkLastAction();
     }
@@ -244,7 +238,7 @@ public class UiWindowsTest extends Tests {
     @SneakyThrows
     void restart() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.restart();
         winPage.checkHistoryRowRestartByPowerOk();
         winPage.checkHistoryRowRestartByPowerErr();
@@ -257,10 +251,9 @@ public class UiWindowsTest extends Tests {
     @SneakyThrows
     void checkCostAfterRestart() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
         winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "равна");
     }
-
 
 
     @Test
@@ -269,7 +262,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Выключить")
     void stopSoft() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.stopSoft();
         winPage.checkHistoryRowTurnOffOk();
         winPage.checkHistoryRowTurnOffErr();
@@ -281,7 +274,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Выключить")
     void checkCostAfterStopSoft() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
         winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "больше");
     }
 
@@ -291,7 +284,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Изменить конфигурацию")
     void changeConfiguration() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.changeConfiguration();
         winPage.checkHistoryRowChangeFlavorOk();
         winPage.checkHistoryRowChangeFlavorErr();
@@ -303,8 +296,8 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Изменить конфигурацию")
     void checkCostAfterChangeConfiguration() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
-        winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "больше");
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
+        winPage.vmOrderTextCompareByKey(costAfterChange, currentCost, "больше");
     }
 
     @Test
@@ -313,7 +306,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Включить")
     void start() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.start();
         winPage.checkHistoryRowTurnOnOk();
         winPage.checkHistoryRowTurnOnErr();
@@ -325,8 +318,8 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Включить")
     void checkStartAfterStart() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
-        winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "больше");
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
+        winPage.vmOrderTextCompareByKey(costAfterChange,currentCost,"больше");
     }
 
     @Test
@@ -335,7 +328,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Добавить диск")
     void discActAdd() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.discActAdd();
         winPage.checkHistoryRowDiscAddOk();
         winPage.checkHistoryRowDiscAddErr();
@@ -347,8 +340,8 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Добавить диск")
     void checkCostAfterDiscActAdd() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
-        winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "больше");
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
+        winPage.vmOrderTextCompareByKey(costAfterChange,currentCost,"больше");
     }
 
     @Test
@@ -357,7 +350,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Отключить в ОС")
     void discActOff() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.discActOff();
         winPage.checkHistoryRowDiscTurnOffOk();
         winPage.checkHistoryRowDiscTurnOffErr();
@@ -369,7 +362,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Отключить в ОС")
     void checkCostAfterTurnOffOs() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
         winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "равна");
     }
 
@@ -379,7 +372,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Подключить в ОС")
     void discActOn() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.discActOn();
         winPage.checkHistoryRowDiscTurnOnOk();
         winPage.checkHistoryRowDiscTurnOnErr();
@@ -391,7 +384,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Подключить в ОС")
     void checkCostAfterConnectOS() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
         winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "равна");
     }
 
@@ -401,7 +394,8 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Отключить в ОС. Удалить диск")
     void discActDelete() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
+        winPage.discActOff();
         winPage.discActDelete();
         winPage.checkHistoryRowDiscDeleteOk();
         winPage.checkHistoryRowDiscDeleteErr();
@@ -413,7 +407,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Проверка стоимости после действия Удалить диск")
     void checkCostAfterDdiscActDelete() {
         WindowsPage winPage = new WindowsPage(product);
-        costAfterChange =winPage.getCostAfterChangeReloadPage(product);
+        costAfterChange = winPage.getCostAfterChangeReloadPage(product);
         winPage.vmOrderTextCompareByKey(currentCost, costAfterChange, "равна");
     }
 
@@ -445,7 +439,7 @@ public class UiWindowsTest extends Tests {
     @DisplayName("UI Windows. Выключить принудительно")
     void stopHard() {
         WindowsPage winPage = new WindowsPage(product);
-        currentCost=winPage.getCurrentCostReloadPage(product);
+        currentCost = winPage.getCurrentCostReloadPage(product);
         winPage.stopHard();
         winPage.checkHistoryRowForceTurnOffOk();
         winPage.checkHistoryRowForceTurnOffErr();
