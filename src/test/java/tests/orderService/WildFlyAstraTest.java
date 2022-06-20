58d44897273bc593225e4b943f2e04044dbc6707
VTB4043675 <rvermakov@dev.vtb.ru>
2022-06-20 13:49:01 +0300

    artemis_astra add

--- src/test/java/tests/orderService/WildFlyAstraTest.java
+++ src/test/java/tests/orderService/WildFlyAstraTest.java
@@ -16,7 +16,7 @@ import tests.Tests;
 @Feature("WildFly (Astra)")
 @Tags({@Tag("regress"), @Tag("orders"), @Tag("wildflyAstra"), @Tag("prod")})
 public class WildFlyAstraTest extends Tests {
-    final String productName = "Разработка WildFly Astra";
+    final String productName = "WildFly Astra";
 
     @TmsLink("833650")
     @Source(ProductArgumentsProvider.PRODUCTS)
