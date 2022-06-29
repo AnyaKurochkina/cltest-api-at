4ffd86daa38b34316d2dd284b10f3bf18de0b3fe
VTB4043675 <rvermakov@dev.vtb.ru>
2022-06-29 14:49:18 +0300

    checkActionStatusMethod() counter 20 -> 30

--- src/test/java/steps/orderService/OrderServiceSteps.java
+++ src/test/java/steps/orderService/OrderServiceSteps.java
@@ -256,7 +256,7 @@ public class OrderServiceSteps extends Steps {
     @Step("Ожидание успешного выполнения action")
     public static void checkActionStatusMethod(String exp_status, IProduct product, String action_id) {
         String actionStatus = "";
-        int counter = 22;
+        int counter = 30;
         log.info("Проверка статуса выполнения действия");
         while ((actionStatus.equals("pending") || actionStatus.equals("changing") || actionStatus.equals("")) && counter > 0) {
             Waiting.sleep(20000);
