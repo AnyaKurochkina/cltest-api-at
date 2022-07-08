d619f6f05d4ce09376bb16c23344707afbce4b01
VTB4043675 <rvermakov@dev.vtb.ru>
2022-07-08 15:20:22 +0300

    urls fix

--- src/test/java/models/authorizer/Folder.java
+++ src/test/java/models/authorizer/Folder.java
@@ -72,7 +72,7 @@ public class Folder extends Entity {
     }
 
     public void edit(){
-        String titleNew = new Http(Configure.IamURL)
+        String titleNew = new Http(Configure.ResourceManagerURL)
                 .body(toJson())
                 .patch("/v1/folders/{}", name)
                 .assertStatus(200)
@@ -86,7 +86,7 @@ public class Folder extends Entity {
     @Step("Создание папки")
     protected void create() {
         String url = kind.equals(BUSINESS_BLOCK) ? "/v1/organizations/vtb/folders" : String.format("/v1/folders/%s/folders", parentId);
-        name = new Http(Configure.IamURL)
+        name = new Http(Configure.ResourceManagerURL)
                 .body(toJson())
                 .post(url)
                 .assertStatus(201)
@@ -97,7 +97,7 @@ public class Folder extends Entity {
     @Override
     @Step("Удаление папки")
     protected void delete() {
-        new Http(Configure.IamURL)
+        new Http(Configure.ResourceManagerURL)
                 .delete("/v1/folders/" + name)
                 .assertStatus(204);
     }
