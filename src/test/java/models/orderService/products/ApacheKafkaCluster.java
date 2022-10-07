7706846fd6c918de88cae8a951c0800c487ed641
VTB4043675 <rvermakov@dev.vtb.ru>
2022-10-07 10:50:08 +0300

    cluster name random kafka

--- src/test/java/models/orderService/products/ApacheKafkaCluster.java
+++ src/test/java/models/orderService/products/ApacheKafkaCluster.java
@@ -21,10 +21,7 @@ import steps.orderService.OrderServiceSteps;
 import steps.references.ReferencesStep;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import static core.utils.Waiting.sleep;
 
@@ -90,7 +87,7 @@ public class ApacheKafkaCluster extends IProduct {
                 .set("$.order.attrs.kafka_version", kafkaVersion)
                 .set("$.order.attrs.layout", getIdGeoDistribution("kafka", "kafka:zookeeper"))
                 .set("$.order.attrs.ad_logon_grants[0].groups[0]", accessGroup.getPrefixName())
-
+                .set("$.order.attrs.cluster_name", "at-" + new Random().nextInt())
                 .remove("$.order.attrs.ad_logon_grants", isTest())
                 //Fix
 
