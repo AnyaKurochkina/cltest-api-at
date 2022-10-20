2a36dd6ecec54827bc874536dd83a55105871ab9
VTB4043675 <rvermakov@dev.vtb.ru>
2022-10-20 23:16:50 +0400

    fix name

--- src/test/java/models/orderService/products/S3Ceph.java
+++ src/test/java/models/orderService/products/S3Ceph.java
@@ -55,7 +55,7 @@ public class S3Ceph extends IProduct {
     public String getBucketName(String name){
         JsonPath path = ResourceManagerSteps.getProjectPath(projectId);
         return path.getString("data.environment_prefix.name") + "-" +
-                path.getString("data.information_system.code")  + "-";
+                path.getString("data.information_system.code")  + "-" + name;
     }
 
     @Step("Удалить бакет")
