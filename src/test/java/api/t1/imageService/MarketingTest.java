b9e86e08292650c02c60e1cac85da3c6cd6dda8e
airat.muzafarov <inkin_88@mail.ru>
2023-01-10 11:27:47 +0300

    PO-723 Доработки параметров в marketing_info

--- src/test/java/api/t1/imageService/MarketingTest.java
+++ src/test/java/api/t1/imageService/MarketingTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static steps.t1.imageService.ImageServiceSteps.*;
+import static steps.t1.imageService.ImageServiceSteps.createMarketing;
 
 @Tag("image_service")
 @Epic("Сервис образов")
@@ -24,10 +25,7 @@ public class MarketingTest extends Tests {
     @TmsLink("1258504")
     @DisplayName("Создание marketing")
     public void createMarketingTest() {
-        Marketing marketing = Marketing.builder()
-                .name("marketing_create_api_test")
-                .build()
-                .createObject();
+        Marketing marketing = createMarketing("marketing_create_api_test");
         assertTrue(isMarketingExist(marketing.getName()));
     }
 
@@ -35,10 +33,7 @@ public class MarketingTest extends Tests {
     @TmsLink("1263334")
     @DisplayName("Получение marketing по id")
     public void getMarketingByIdTest() {
-        Marketing marketing = Marketing.builder()
-                .name("get_marketing_by_id_api_test")
-                .build()
-                .createObject();
+        Marketing marketing = createMarketing("get_marketing_by_id_api_test");
         Marketing marketingById = getMarketingById(marketing.getId());
         assertEquals(marketing, marketingById);
     }
@@ -47,10 +42,7 @@ public class MarketingTest extends Tests {
     @TmsLink("1258511")
     @DisplayName("Получение списка marketing")
     public void getMarketingListTest() {
-        Marketing marketing = Marketing.builder()
-                .name("get_marketing_list_api_test")
-                .build()
-                .createObject();
+        Marketing marketing = createMarketing("get_marketing_list_api_test");
         List<Marketing> marketingList = getMarketingList();
         assertFalse(marketingList.isEmpty(), "Список marketing пустой");
         assertTrue(isMarketingExist(marketing.getName()), String.format("Marketing с именем %s не найден", marketing.getName()));
@@ -60,10 +52,7 @@ public class MarketingTest extends Tests {
     @TmsLink("1258548")
     @DisplayName("Частичное обновление marketing")
     public void partialUpdateMarketingListTest() {
-        Marketing marketing = Marketing.builder()
-                .name("partial_update_marketing_list_api_test")
-                .build()
-                .createObject();
+        Marketing marketing = createMarketing("partial_update_marketing_list_api_test");
         partialUpdateMarketingById(marketing.getId(), new JSONObject().put("description", "test")
                 .put("name", marketing.getName()));
         Marketing marketingById = getMarketingById(marketing.getId());
@@ -74,10 +63,7 @@ public class MarketingTest extends Tests {
     @TmsLink("1263320")
     @DisplayName("Удаление marketing по id")
     public void deleteMarketingTest() {
-        Marketing marketing = Marketing.builder()
-                .name("delete_marketing_api_test")
-                .build()
-                .createObject();
+        Marketing marketing = createMarketing("delete_marketing_api_test");
         deleteMarketingById(marketing.getId());
         assertFalse(isMarketingExist(marketing.getName()));
     }
