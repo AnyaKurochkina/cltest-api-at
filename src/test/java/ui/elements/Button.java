950f7759e3daa57bc2e790b45eeb8c2503bb2a4f
Alexey Kosovskiy <al.kosovsky@gmail.com>
2023-02-14 10:16:48 +0300

    Редизайн модификаторов

--- src/test/java/ui/elements/Button.java
+++ src/test/java/ui/elements/Button.java
@@ -36,6 +36,11 @@ public class Button implements TypifiedElement {
         return new Button($x("//button[@aria-label='{}']", value));
     }
 
+    @Step("Получение Button по id '{id}'")
+    public static Button byId(String id) {
+        return new Button($x("//button[@id='{}']", id));
+    }
+
     public static Button byXpath(@Language("XPath") String xPath) {
         return new Button($x(xPath));
     }
