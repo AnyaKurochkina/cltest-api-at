866b1a61061a8e9ae30e9132b502314704703ced
VTB4043675 <rvermakov@dev.vtb.ru>
2023-04-07 10:27:37 +0300

    вынесен maximized в проперти

--- src/test/java/org/junit/TestsExecutionListener.java
+++ src/test/java/org/junit/TestsExecutionListener.java
@@ -59,7 +59,7 @@ public class TestsExecutionListener implements TestExecutionListener {
         baseUrl = URL;
         isRemote();
         Configuration.browserSize = "1920x1080";
-        Configuration.startMaximized = true;
+        Configuration.startMaximized = Boolean.parseBoolean(Configure.getAppProp("webdriver.maximized", "false"));
         Configuration.pageLoadTimeout = 60000;
         Configuration.timeout = 45000;
         Configuration.driverManagerEnabled = false;
