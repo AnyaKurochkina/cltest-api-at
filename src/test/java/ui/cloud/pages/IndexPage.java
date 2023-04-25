23a67f4b4dc40660d019c6dd2d7316751756cc5f
Alexey Kosovskiy <al.kosovsky@gmail.com>
2023-04-25 11:24:53 +0300

    Старое меню

--- src/test/java/ui/cloud/pages/IndexPage.java
+++ src/test/java/ui/cloud/pages/IndexPage.java
@@ -1,7 +1,9 @@
 package ui.cloud.pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import core.helper.StringUtils;
 import core.utils.Waiting;
 import io.qameta.allure.Step;
 import lombok.Getter;
@@ -16,40 +18,24 @@ import static com.codeborne.selenide.Selenide.$x;
 @Getter
 public class IndexPage {
 
+    final SelenideElement linkServicesList = StringUtils.$x("//a[.='Список сервисов']");
     private final SelenideElement orderMoreBtn = $x("//button[contains(., 'Заказать еще')]");
-    private final SelenideElement createOrderButton = $x("//div[@data-testid='order-list-add-button']//button");
-    private final SelenideElement instrumentsMenuItem = $x("//div[text()='Инструменты']");
-    private final SelenideElement allServicesMenuItem = $x("//div[text()='Все сервисы']");
-    private final SelenideElement analyticsMenuItem = $x("//div[text()='Аналитика']");
-    private final SelenideElement auditMenuItem = $x("//div[text()='Аудит']");
-    private final SelenideElement allResourcesMenuItem = $x("//div[text()='Все ресурсы']");
-    private final SelenideElement servicesMenuItem = $x("//div[text()='Сервисы']");
-    private final SelenideElement servicesListMenuItem = $x("//div[text()='Список сервисов']");
-    private final SelenideElement mainLogo = $x("//img");
-
-    public IndexPage() {
-        mainLogo.hover();
-        allServicesMenuItem.shouldBe(Condition.visible);
-    }
+    private final SelenideElement portalAuditLink = $x("//a[@href='/analytics/audit']");
 
     public ProductsPage clickOrderMore() {
-        allResourcesMenuItem.click();
-        createOrderButton.shouldBe(activeCnd).hover().shouldBe(clickableCnd).click();
+        orderMoreBtn.shouldBe(activeCnd).hover().shouldBe(clickableCnd).click();
         return new ProductsPage();
     }
 
     @Step("Переход на страницу 'Список сервисов'")
     public ServicesListPage goToServicesListPage() {
-        servicesMenuItem.hover();
-        servicesListMenuItem.shouldBe(activeCnd).hover().shouldBe(clickableCnd).click();
+        linkServicesList.shouldBe(activeCnd).hover().shouldBe(clickableCnd).click();
         return new ServicesListPage();
     }
 
     @Step("Переход на страницу Аналитика.Аудит")
     public AuditPage goToPortalAuditPage() {
-        instrumentsMenuItem.click();
-        analyticsMenuItem.hover();
-        auditMenuItem.click();
+        portalAuditLink.shouldBe(Condition.visible).click();
         Waiting.sleep(500);
         return new AuditPage();
     }
