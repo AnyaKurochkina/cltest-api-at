08c6d18d80b8f73385a78d2445676803f7ea8cab
Alexey Kosovskiy <al.kosovsky@gmail.com>
2023-04-28 15:20:53 +0300

    Ограничения продукта

--- src/test/java/ui/cloud/tests/orders/ProductsTest.java
+++ src/test/java/ui/cloud/tests/orders/ProductsTest.java
@@ -6,10 +6,13 @@ import core.enums.Role;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import io.qameta.allure.TmsLinks;
+import models.cloud.authorizer.GlobalUser;
 import models.cloud.authorizer.Project;
 import models.cloud.authorizer.ProjectEnvironmentPrefix;
 import models.cloud.productCatalog.product.Product;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.DisabledIfEnv;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +23,9 @@ import ui.cloud.pages.IndexPage;
 import ui.cloud.pages.orders.ProductsPage;
 import ui.extesions.ConfigExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static steps.productCatalog.ProductSteps.createProduct;
@@ -27,6 +33,7 @@ import static steps.productCatalog.ProductSteps.partialUpdateProductByName;
 import static ui.elements.TypifiedElement.refresh;
 
 @Feature("Маркетплейс продуктов для заказа")
+@DisabledIfEnv("prod")
 @ExtendWith(ConfigExtension.class)
 public class ProductsTest extends Tests {
 
@@ -75,5 +82,55 @@ public class ProductsTest extends Tests {
         page.expandProductsList();
         int index2 = page.getProducts().indexOf(page.getProducts().find(Condition.exactText(name)));
         assertTrue(index1 < index2);
+    }
+
+    @Test
+    @TmsLinks({@TmsLink("1363579"), @TmsLink("1363846")})
+    @DisplayName("Ограничение продукта по пользователям")
+    void checkProductRestrictionByUsername() {
+        String name = "at_ui_check_product_role_restriction";
+        GlobalUser pcAdmin = GlobalUser.builder().role(Role.PRODUCT_CATALOG_ADMIN).build().createObject();
+        GlobalUser orderServiceAdmin = GlobalUser.builder().role(Role.ORDER_SERVICE_ADMIN).build().createObject();
+        createProduct(name, name);
+        partialUpdateProductByName(name, new JSONObject().put("is_open", "true").put("in_general_list", "true")
+                .put("allowed_groups", Collections.singletonList(pcAdmin.getUsername())));
+        ProductsPage page = new IndexPage().clickOrderMore();
+        page.expandProductsList();
+        assertFalse(page.isProductDisplayed(name));
+        partialUpdateProductByName(name, new JSONObject().put("allowed_groups",
+                Arrays.asList(pcAdmin.getUsername(), orderServiceAdmin.getUsername())));
+        refresh();
+        page.expandProductsList();
+        assertTrue(page.isProductDisplayed(name));
+        partialUpdateProductByName(name, new JSONObject().put("allowed_groups", new JSONArray())
+                .put("restricted_groups", Collections.singletonList(pcAdmin.getUsername())));
+        refresh();
+        page.expandProductsList();
+        assertTrue(page.isProductDisplayed(name));
+        partialUpdateProductByName(name, new JSONObject().put("restricted_groups",
+                Arrays.asList(pcAdmin.getUsername(), orderServiceAdmin.getUsername())));
+        refresh();
+        page.expandProductsList();
+        assertFalse(page.isProductDisplayed(name));
+    }
+
+    @Test
+    @TmsLinks({@TmsLink("513987"), @TmsLink("1291555")})
+    @DisplayName("Ограничение продукта по ролям keycloak")
+    //Для учётки должна быть добавлена роль "superadmin-product_catalog" в Keycloak
+    void checkProductRestrictionByKeycloakRole() {
+        String name = "at_ui_check_product_role_restriction";
+        String keycloakRole = "superadmin-product_catalog";
+        createProduct(name, name);
+        partialUpdateProductByName(name, new JSONObject().put("is_open", "true").put("in_general_list", "true")
+                .put("allowed_groups", Collections.singletonList(keycloakRole)));
+        ProductsPage page = new IndexPage().clickOrderMore();
+        page.expandProductsList();
+        assertTrue(page.isProductDisplayed(name));
+        partialUpdateProductByName(name, new JSONObject().put("allowed_groups", new JSONArray())
+                .put("restricted_groups", Collections.singletonList(keycloakRole)));
+        refresh();
+        page.expandProductsList();
+        assertFalse(page.isProductDisplayed(name));
     }
 }
