1db7fc0c913f6102aaab91e9bb10d41c3167111f
Alexey Kosovskiy <al.kosovsky@gmail.com>
2023-08-14 18:07:19 +0300

    Фиксы

--- src/test/java/ui/cloud/tests/productCatalog/allowedAction/DeleteAllowedActionTest.java
+++ src/test/java/ui/cloud/tests/productCatalog/allowedAction/DeleteAllowedActionTest.java
@@ -2,41 +2,31 @@ package ui.cloud.tests.productCatalog.allowedAction;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ui.cloud.pages.ControlPanelIndexPage;
 
-import static steps.productCatalog.AllowedActionSteps.createAllowedAction;
-
 @Feature("Удаление разрешенного действия")
 public class DeleteAllowedActionTest extends AllowedActionBaseTest {
-
-    @Override
-    @BeforeEach
-    public void setUp() {
-    }
 
     @Test
     @TmsLink("1247553")
     @DisplayName("Удаление разрешенного действия из списка")
     public void deleteAllowedActionOnList() {
-        String name = createAllowedAction(TITLE).getName();
         new ControlPanelIndexPage()
                 .goToAllowedActionsListPage()
-                .delete(name)
-                .checkAllowedActionNotFound(name);
+                .delete(NAME)
+                .checkAllowedActionNotFound(NAME);
     }
 
     @Test
     @TmsLink("1599361")
     @DisplayName("Удаление разрешенного действия со страницы")
     public void deleteAllowedActionOnPage() {
-        String name = createAllowedAction(TITLE).getName();
         new ControlPanelIndexPage()
                 .goToAllowedActionsListPage()
-                .openAllowedActionPage(name)
+                .openAllowedActionPage(NAME)
                 .delete()
-                .checkAllowedActionNotFound(name);
+                .checkAllowedActionNotFound(NAME);
     }
 }
