a76e2ca106c2b9e14e7fbac94b1407cd4117ef17
Airat Muzafarov <airat.muzafarov@gmail.com>
2023-10-02 11:48:29 +0300

    new fields added

--- src/test/java/api/t1/HealthCheckTest.java
+++ src/test/java/api/t1/HealthCheckTest.java
@@ -1,12 +1,15 @@
 package api.t1;
 
 import api.Tests;
+import core.enums.Role;
+import core.helper.http.Http;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static core.helper.Configure.*;
 
@@ -17,6 +20,15 @@ public class HealthCheckTest extends Tests {
 
     @Test
     public void healthCheckTest() {
-        Arrays.asList(StateServiceURL, ProductCatalogURL, TarifficatorURL, )
+        List<String> urls = Arrays.asList(StateServiceURL, ProductCatalogURL, TarifficatorURL, CalculatorURL, IamURL, PortalBackURL, OrderServiceURL,
+                Day2ServiceURL, ReferencesURL, RestrictionServiceUrl, SyncService, Budget);
+        for (String url : urls) {
+             new Http(url)
+                    .setRole(Role.CLOUD_ADMIN)
+                    .get("/api/v1/health")
+                    .assertStatus(200)
+                    .jsonPath()
+                    .getString("status");
+        }
     }
 }
