fc08c69bc016010b7d0a0c808fb285fb9b31e77f
VTB4043675 <rvermakov@dev.vtb.ru>
2023-11-07 10:49:01 +0300

    fix TarantoolDataGrid

--- src/test/java/models/cloud/orderService/products/TarantoolDataGrid.java
+++ src/test/java/models/cloud/orderService/products/TarantoolDataGrid.java
@@ -30,7 +30,6 @@ public class TarantoolDataGrid extends IProduct {
     public static final String SERVICE_PATH = "data.find{it.type=='cluster'}.data.config.cluster.find{it.instance=='%s'}.state";
     @ToString.Include
     String osVersion;
-    Flavor flavor;
     String tarantoolVersion;
 
     @Override
@@ -50,8 +49,6 @@ public class TarantoolDataGrid extends IProduct {
             setDomain(OrderServiceSteps.getDomain(this));
         if (tarantoolVersion == null)
             tarantoolVersion = getRandomProductVersionByPathEnum("tarantool_version.enum");
-        if (flavor == null)
-            flavor = getMinFlavor();
         return this;
     }
 
@@ -67,7 +64,6 @@ public class TarantoolDataGrid extends IProduct {
         return JsonHelper.getJsonTemplate(jsonTemplate)
                 .set("$.order.product_id", productId)
                 .set("$.order.attrs.domain", getDomain())
-                .set("$.order.attrs.flavor", new JSONObject(flavor.toString()))
                 .set("$.order.attrs.default_nic.net_segment", getSegment())
                 .set("$.order.attrs.availability_zone", getAvailabilityZone())
                 .set("$.order.attrs.platform", getPlatform())
