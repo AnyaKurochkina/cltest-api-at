8030d60f9e2f801db7ca815c2c569d0c0690278a
VTB4043675 <rvermakov@dev.vtb.ru>
2023-11-14 18:08:09 +0300

    mm tests

---
+++ src/test/java/api/cloud/defectolog/DefectologMMTest.java
@@ -0,0 +1,65 @@
+package api.cloud.defectolog;
+
+import api.cloud.defectolog.models.DefectPage;
+import api.cloud.defectolog.models.StartTask;
+import api.cloud.defectolog.steps.DefectologSteps;
+import api.cloud.tagService.AbstractTagServiceTest;
+import api.cloud.tagService.v2.InventoryFilterV2Test;
+import core.exception.NotFoundElementException;
+import core.utils.Waiting;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import models.cloud.tagService.Context;
+import models.cloud.tagService.Inventory;
+import models.cloud.tagService.TagServiceSteps;
+import models.cloud.tagService.v2.InventoryTagsV2;
+import org.junit.jupiter.api.*;
+import ui.t1.tests.engine.EntitySupplier;
+
+import java.time.Duration;
+import java.util.*;
+
+import static models.cloud.tagService.TagServiceSteps.inventoryTagsV2;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Epic("Дефектолог")
+@Feature("Проверка создания дефектов")
+public class DefectologMMTest extends AbstractTagServiceTest {
+
+    @Test
+    @DisplayName("Тег sys_item_maintenance_mode. not_exists_attrs")
+    void notExistsAttrs() {
+        Inventory inventory = generateInventories(1).get(0);
+
+        StartTask task = StartTask.builder().kwargsParam(StartTask.KwargsParam.builder()
+                .taskValidators(Collections.singletonList("INV-MM-ATTRS")).build()).build();
+        Assertions.assertTrue(DefectologSteps.tasksCreate(task));
+
+        int defectId = findDefectIdByInternalName("INV-MM-ATTRS");
+        DefectPage defectPage = readDefectPage(defectId);
+
+//        TagServiceSteps.inve
+    }
+
+
+    private void assertDefectPageContainsInventories(String internalName, List<Inventory> inventories) {
+        int defectId = findDefectIdByInternalName(internalName);
+        DefectPage defectPage = readDefectPage(defectId);
+        inventories.forEach(inventory ->
+                Assertions.assertTrue(defectPage.getPatients().contains(inventory.getId()),
+                        String.format("Inventory %s not found in defectPage %d ", inventory.getId(), defectPage.getId())));
+    }
+
+    private int findDefectIdByInternalName(String internalName) {
+        return DefectologSteps.defectsList().stream()
+                .filter(e -> Duration.between(e.getCreatedAt().toInstant(), new Date().toInstant()).getSeconds() < 10)
+                .filter(e -> e.getGroup().getInternalName().equals(internalName))
+                .findFirst()
+                .orElseThrow(NotFoundElementException::new).getId();
+    }
+
+    private DefectPage readDefectPage(int defectId) {
+        int pageId = DefectologSteps.defectsRead(defectId).getDefectPages().get(0).getId();
+        return DefectologSteps.defectPagesRead(pageId);
+    }
+}
