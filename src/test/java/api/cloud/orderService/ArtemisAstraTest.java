76bd9840059e8b8a8018594535e917d00bf7ff4a
VTB4043675 <rvermakov@dev.vtb.ru>
2024-02-16 14:40:55 +0300

    Добавлены проверки для теста Обновление сертификатов Artemis по задаче USPA-1534

--- src/test/java/api/cloud/orderService/ArtemisAstraTest.java
+++ src/test/java/api/cloud/orderService/ArtemisAstraTest.java
@@ -1,29 +1,37 @@
 package api.cloud.orderService;
 
 import api.Tests;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.TmsLink;
-import io.qameta.allure.TmsLinks;
+import core.helper.StringUtils;
+import core.utils.AssertUtils;
+import core.utils.ssh.SshClient;
+import io.qameta.allure.*;
+import lombok.SneakyThrows;
 import models.cloud.orderService.products.Artemis;
 import models.cloud.subModels.Flavor;
-import org.junit.DisabledIfEnv;
-import org.junit.MarkDelete;
-import org.junit.ProductArgumentsProvider;
-import org.junit.Source;
+import org.junit.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
+import steps.orderService.OrderServiceSteps;
 
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 
 @Epic("Продукты")
 @Feature("Artemis Astra")
 @Tags({@Tag("regress"), @Tag("orders"), @Tag("artemis_astra"), @Tag("prod")})
 @DisabledIfEnv("ift")
+@Execution(ExecutionMode.SAME_THREAD)
 public class ArtemisAstraTest extends Tests {
+
+    @Mock
+    public static Artemis loadBalancer = Artemis.builder().platform("OpenStack").env("DEV").segment("dev-srv-app").build().buildFromLink("https://prod-portal-front.cloud.vtb.ru/all/orders/c1dfe43f-8f3c-4d54-b280-6981c1fca2dc/main?context=proj-ln4zg69jek&type=project&org=vtb");
+
 
     @TmsLink("982658")
     @Source(ProductArgumentsProvider.PRODUCTS)
@@ -129,10 +137,7 @@ public class ArtemisAstraTest extends Tests {
                     Integer cpu = Integer.parseInt(artemis.executeSsh(client, "cat /proc/cpuinfo | grep processor | wc -l"));
                     String memConfig = artemis.executeSsh(client, "sudo cat /app/etc/systemd/artemis.service.conf | grep \"Xmx\"");
 
-                    Assertions.assertAll("Проверка изменений по SSH на vm " + client.getHost(),
-                            () -> Assertions.assertEquals(flavor.getMemory(), memory, "Размер ОЗУ не изменился"),
-                            () -> Assertions.assertEquals(flavor.getCpus(), cpu, "Размер CPU не изменился"),
-                            () -> Assertions.assertEquals(memConfig, "-Xmx" + flavor.getMemory() / 4 + "G", "Размер не mem / 4"));
+                    Assertions.assertAll("Проверка изменений по SSH на vm " + client.getHost(), () -> Assertions.assertEquals(flavor.getMemory(), memory, "Размер ОЗУ не изменился"), () -> Assertions.assertEquals(flavor.getCpus(), cpu, "Размер CPU не изменился"), () -> Assertions.assertEquals(memConfig, "-Xmx" + flavor.getMemory() / 4 + "G", "Размер не mem / 4"));
                 });
             }
         }
@@ -215,6 +220,12 @@ public class ArtemisAstraTest extends Tests {
     void updateCertsArtemis(Artemis product, Integer num) {
         try (Artemis artemis = product.createObjectExclusiveAccess()) {
             artemis.updateCertsArtemis();
+            if (artemis.isDev()) {
+                artemis.runOnAllNodesBySsh(client -> Assertions.assertAll("Проверка изменений по SSH на vm " + client.getHost(),
+                        () -> assertCertificateNameMatches(artemis, client),
+                        () -> assertCertificateStartDateMatches(artemis, client),
+                        () -> assertCertificateEndDateMatches(artemis, client)));
+            }
         }
     }
 
@@ -237,5 +248,34 @@ public class ArtemisAstraTest extends Tests {
         try (Artemis artemis = product.createObjectExclusiveAccess()) {
             artemis.deleteObject();
         }
+    }
+
+    @SneakyThrows
+    void asserDate(String expectedDate, String actualDate, String message) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        Date expectedDateTime = formatter.parse(expectedDate);
+        Date actualDateTime = formatter.parse(actualDate);
+        AssertUtils.AssertDate(expectedDateTime, actualDateTime, 5, message);
+    }
+
+    @Step("[Проверка] Обновление имени сертификата")
+    void assertCertificateNameMatches(Artemis artemis, SshClient client) {
+        String cnOrderService = OrderServiceSteps.getObjectClass(artemis, "data.find{it.type=='cluster'}.data.config.cert_cn", String.class);
+        String cn = artemis.executeSsh(client, StringUtils.format("sudo openssl x509 -noout -subject -in '/app/itc/certs/{}.corp.dev.vtb-vtb-artemis.pem' | awk --field-separator '=' '{print $(NF)}' | xargs", client.getHost()));
+        Assertions.assertEquals(cnOrderService, cn, "Имя сертификата не совпадает");
+    }
+
+    @Step("[Проверка] Обновление даты начала сертификата")
+    void assertCertificateStartDateMatches(Artemis artemis, SshClient client) {
+        String startDateOrderService = OrderServiceSteps.getObjectClass(artemis, "data.find{it.type=='cluster'}.data.config.cert_start_date", String.class);
+        String startDate = artemis.executeSsh(client, StringUtils.format("date --date=\"$(sudo openssl x509 -startdate -noout -in '/app/itc/certs/{}.corp.dev.vtb-vtb-artemis.pem' | cut -d= -f 2)\" -Is -u", client.getHost()));
+        asserDate(startDateOrderService, startDate, "Дата начала сертификата не совпадает");
+    }
+
+    @Step("[Проверка] Обновление даты окончания сертификата")
+    void assertCertificateEndDateMatches(Artemis artemis, SshClient client) {
+        String endDateOrderService = OrderServiceSteps.getObjectClass(artemis, "data.find{it.type=='cluster'}.data.config.cert_end_date", String.class);
+        String endDate = artemis.executeSsh(client, StringUtils.format("date --date=\"$(sudo openssl x509 -enddate -noout -in '/app/itc/certs/{}.corp.dev.vtb-vtb-artemis.pem' | cut -d= -f 2)\" -Is -u", client.getHost()));
+        asserDate(endDateOrderService, endDate, "Дата окончания сертификата не совпадает");
     }
 }
